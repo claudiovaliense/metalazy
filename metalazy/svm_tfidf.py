93bd5f9039a7e0d90f6a669afadfe71e6f362f2d
Claudio Valiense <claudiovaliense@gmail.com>
2019-11-25 18:06:38 -0300

    metricas

--- metalazy/svm_tfidf.py
+++ metalazy/svm_tfidf.py
@@ -6,16 +6,14 @@ from sklearn.metrics import f1_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
 import numpy
-from BayesianOptimization import BayesianOptimization
 import timeit  # Measure time
 from hyperopt import hp
-import sys 
-
-sys.path.append("../../doutorado") # Import directory
+import sys # Import other directory
+sys.path.append("../../doutorado") 
 import claudio_funcoes as cv  # Functions utils author
 
-#name_dataset="stanford_tweets"
-#name_dataset="reut"
+"""Example execution: python3.6 stanford_tweets linear"""
+
 name_dataset=sys.argv[1]
 kernel=sys.argv[2]
 
@@ -34,27 +32,63 @@ dir_test = [
     "/home/claudiovaliense/dataset/" + name_dataset +"/representations/5-folds/TFIDF_removed_stopwords_mindf1/extract/test/test3",
     "/home/claudiovaliense/dataset/" + name_dataset +"/representations/5-folds/TFIDF_removed_stopwords_mindf1/extract/test/test4"]
 
-f1_micro=[]
-f1_macro=[]
+y_test_folds = []
+y_pred_folds = []
+best_param_folds=[]
+tunned_param_lbd = {'kernel': kernel, 'verbose': False, 'probability': False,
+                    'degree': 3, 'shrinking': True,
+                    'decision_function_shape': None, 
+                    'tol': 0.001, 'cache_size': 25000, 'coef0': 0.0, 'gamma': 'auto',
+                    'class_weight': None, 'random_state': 42}
+
+C = numpy.append(2.0 ** numpy.arange(-5, 15, 2), 1)    
+#tuned_parameters = [{'kernel': ['linear'], 'C': [1,2, 3,4,5,10, 100, 1000, 1500,5000]}, 
+#							{'kernel': ['rbf'], 'gamma': [2.5, 2, 1.5, 1, 1e-1, 1e-2, 1e-3, 1e-4],
+#			       			     'C': [1, 10, 100, 1000, 1500,5000, 10000]}]
+
+tuned_parameters = [{'kernel': ['linear'], 'C': C}, 
+                   {'kernel': ['rbf'], 'gamma': [2.5, 2, 1.5, 1, 1e-1, 1e-2, 1e-3, 1e-4, 'auto'], 'C': C}]
+
+
+#C = [0.0001, 0.001, 0.01, 0.1, 1, 2, 3, 5, 10, 20, 100, 1000, 10000,100000]
+tuned_parameters_svm = [{'C': C}]
+    
 for index_file in range(5):
     model_svm = svm.SVC()
-    model_svm.__init__(**{'kernel': kernel, 'C': 1, 'verbose': False, 'probability': False,
-                    'degree': 3, 'shrinking': True,
-                    'decision_function_shape': None, 'random_state': None,
-                    'tol': 0.001, 'cache_size': 25000, 'coef0': 0.0, 'gamma': 'auto',
-                    'class_weight': None, 'random_state': 42})
-
+    model_svm.__init__(**tunned_param_lbd)    
     x_train, y_train, x_test, y_test = load_svmlight_files([open(dir_train[index_file], 'rb'), open(dir_test[index_file], 'rb')])
-    #print('Shape', x_train.shape)
-    #print(x_train)
-    #exit()
-    
-    C = numpy.append(2.0 ** numpy.arange(-5, 15, 2), 1)
-    #C = 2.0 ** numpy.arange(-5, 15, 2)
-    tuned_parameters_svm = [{'C': C}]
 
-    # BayesianOptimization-------
-    '''kernels = ["linear"]
+    # best param svm grid 
+    grid_svm = GridSearchCV(model_svm, tuned_parameters_svm,  cv=3, scoring='f1_macro', n_jobs=1)        
+    grid_svm.fit(x_train, y_train)   
+    print(grid_svm.get_params() )
+    best_param_svm = grid_svm.best_params_  
+    best_param_folds.append(best_param_svm)              
+    y_pred = grid_svm.predict(x_test)
+    #---------    
+    
+    y_test_folds.append(y_test)    
+    y_pred_folds.append(y_pred)                
+
+print(tunned_param_lbd)
+cv.statistics_experiment(y_test_folds, y_pred_folds, best_param_folds)
+print("Time End: %f" % (timeit.default_timer() - ini))
+
+
+'''
+#print('Shape', x_train.shape)
+
+print('Accuracy: ', metrics.accuracy_score(y_test, y_pred))
+precision, recall, fscore, support = score(y_test, y_pred)
+print('precision: {}'.format(precision))
+print('recall: {}'.format(recall))
+print('fscore: {}'.format(fscore))
+print('support: {}'.format(support))
+'''
+# BayesianOptimization-------
+'''
+#from BayesianOptimization import BayesianOptimization
+kernels = ["linear"]
     hyperparameters = {
         "C": hp.uniform("C", 1, 20) #,
         #"kernel": hp.choice("kernel", kernels)
@@ -64,45 +98,7 @@ for index_file in range(5):
     print('Best param: ', best_param_bayesian)
     model_svm.set_params(**best_param_bayesian)
     model_svm.fit(x_train, y_train)
-    y_pred = model_svm.predict(x_test)'''
-    #----------
-
-    
-    # best param svm grid---
-    grid_svm = GridSearchCV(model_svm, tuned_parameters_svm,  cv=3, scoring='f1_macro', n_jobs=-1)
-    grid_svm.fit(x_train, y_train)
-    best_param_svm = grid_svm.best_params_            
-    print('best param: ', best_param_svm)
-    y_pred = grid_svm.predict(x_test)
-    #---------
-    
-    f1_macro.append(f1_score(y_test, y_pred, average='macro'))
-    f1_micro.append(f1_score(y_test, y_pred, average='micro'))
-
-media_f1_macro = sum(f1_macro)/5
-media_f1_micro = sum(f1_micro)/5
-# desvio padrão
-sum_f1_macro=0
-sum_f1_micro=0
-for index in range(5):
-    sum_f1_macro += (f1_macro[index]-media_f1_macro)**(2)
-    sum_f1_micro += (f1_micro[index] - media_f1_micro) ** (2)
-
-print("F1 macro por fold: ", f1_macro)
-print("F1 micro por fold: ", f1_micro)
-print('Média Macro F1: ', media_f1_macro)
-print("Desvio padrão Macro F1: ", (sum_f1_macro/5)**(1/2))
-print('Média Micro F1:  ', media_f1_micro)
-print("Desvio padrão Micro F1: ", (sum_f1_micro/5)**(1/2))
-
-
-
+    y_pred = model_svm.predict(x_test)
 '''
-print('Accuracy: ', metrics.accuracy_score(y_test, y_pred))
-precision, recall, fscore, support = score(y_test, y_pred)
-print('precision: {}'.format(precision))
-print('recall: {}'.format(recall))
-print('fscore: {}'.format(fscore))
-print('support: {}'.format(support))
-'''
-print("Time End: %f" % (timeit.default_timer() - ini))
+#----------
+
